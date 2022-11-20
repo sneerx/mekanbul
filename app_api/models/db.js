f830505918ccf35cf296cbda0bd5a53104a2abbc
sneerx <rizaer14@gmail.com>
2022-11-20 20:42:24 +0300

    odev5

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,6 +1,6 @@
 var mongoose = require("mongoose");
 require("./mekansema");
-var dbURI ="mongodb+srv://riza:mekanbul32@mekanbul.uj7u12r.mongodb.net/?retryWrites=true&w=majority";
+var dbURI ="mongodb+srv://riza:mekanbul32@mekanbul.uj7u12r.mongodb.net/mekanbul?retryWrites=true&w=majority";
 mongoose.connect(dbURI);
 
 function kapat(msg,callback){
