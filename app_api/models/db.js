d9d2d0c42411e57a7a157240abefdd0d8e0138a9
sneerx <rizaer14@gmail.com>
2022-11-20 19:47:27 +0300

    odev5

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,6 +1,6 @@
 var mongoose = require("mongoose");
 require("./mekansema");
-var dbURI ="mongodb+srv://riza:mekanbul32@mekanbul.uj7u12r.mongodb.net/?retryWrites=true&w=majority";
+var dbURI ="mongodb://localhost/mekanbul";
 mongoose.connect(dbURI);
 
 function kapat(msg,callback){
@@ -16,14 +16,14 @@ process.on("SIGINT",function(){
 });
 mongoose.connection.on(
    "connected",function(){
-      console.log(dbURI + "adresindeki veritabanına bağlandı.");
+      console.log(dbURI + " adresindeki veritabanına bağlandı.");
 
    }
 
 );
 mongoose.connection.on(
    "disconnected",function(){
-      console.log(dbURI + "adresindeki veritabanına bağlantısı koptu.");
+      console.log(dbURI + " adresindeki veritabanına bağlantısı koptu.");
 
    }
 
